f445d7f7cf3c23dd4c674fbdaec3cb4b71640608
Lakhte <lakhteagha@gmail.com>
2021-01-25 17:27:15 -0500

    finished up to p04

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -1,18 +1,29 @@
+require 'set'
+
 class HashSet
   attr_reader :count
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @set = Set.new
   end
 
   def insert(key)
+    resize! if @count == num_buckets
+    @set.add(key)
+    @count += 1
   end
 
   def include?(key)
+    @set.include?(key)   
   end
 
   def remove(key)
+    if include?(key)
+      @set.delete(key)
+      @count -= 1
+    end
   end
 
   private
@@ -26,5 +37,8 @@ class HashSet
   end
 
   def resize!
+    num_buckets.times do
+      @store << []
+    end
   end
 end
--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -20,30 +20,54 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Node.new
+    @tail = @head
+    @head.next = @tail
+    @tail.prev = @head
+    @count = 0
   end
 
   def [](i)
-    each_with_index { |link, j| return link if i == j }
+    self.each_with_index { |link, j| return link if i == j }
     nil
   end
 
   def first
+    @head
   end
 
   def last
+    @tail
   end
 
   def empty?
+    @head.key.nil?
   end
 
   def get(key)
+    self.each_with_index { |link, i| return link.val if link.key == key }
+    nil
   end
 
   def include?(key)
+
   end
 
-  def append(key, val)
+  def append(key, val)      # a(head) -> b -> new_node(tail)
+    if @head.key.nil?
+      new_node = Node.new(key, val)
+      @head = new_node
+      @tail = new_node
+      @count += 1
+    else
+      new_node = Node.new(key, val)
+      @tail.next = new_node
+      new_node.prev = @tail
+      @tail = new_node
+      @count +=1 
+    end
   end
 
   def update(key, val)
@@ -53,6 +77,13 @@ class LinkedList
   end
 
   def each
+    arr = []
+    current_node = @head
+    while arr.length < 3
+      arr << current_node.val
+      current_node = current_node.next
+    end
+    arr
   end
 
   # uncomment when you have `each` working and `Enumerable` included
